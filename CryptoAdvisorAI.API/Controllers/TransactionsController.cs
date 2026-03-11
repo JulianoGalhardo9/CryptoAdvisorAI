0009c37dd8fa4dbbee793124828d97486e40b9bd
Juliano Galhardo de Oliveira <julianogalhardo@MacBook-Air-de-Juliano.local>
2026-03-10 23:07:01 -0300

    feat: add TransactionsController and create transaction endpoint

---
+++ CryptoAdvisorAI.API/Controllers/TransactionsController.cs
@@ -0,0 +1,46 @@
+﻿using CryptoAdvisorAI.Application.DTOs;
+using CryptoAdvisorAI.Domain.Entities;
+using CryptoAdvisorAI.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoAdvisorAI.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // O endereço será: api/transactions
+    public class TransactionsController : ControllerBase
+    {
+        private readonly ITransactionRepository _repository;
+
+        // Injeção de Dependência: Pedimos o Repositório para o .NET
+        public TransactionsController(ITransactionRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTransactionRequest request)
+        {
+            //Transformamos o DTO na Entidade (regras de negócio)
+            var transaction = new Transaction(
+                request.Symbol,
+                request.Quantity,
+                request.PriceAtPurchase,
+                request.PurchaseDate
+            );
+
+            // repositório salvar no Postgres
+            await _repository.AddAsync(transaction);
+
+            //Respondemos "200 OK" com a transação criada
+            return Ok(transaction);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var transactions = await _repository.GetAllAsync();
+            return Ok(transactions);
+        }
+    }
+}
+
---
+++ CryptoAdvisorAI.Application/DTOs/CreateTransactionRequest.cs
@@ -0,0 +1,13 @@
+﻿//Objeto usado para transportar dados entre camadas
+
+namespace CryptoAdvisorAI.Application.DTOs
+{
+    // Este é o "contrato" do que o usuário deve enviar no JSON
+    public record CreateTransactionRequest(
+        string Symbol,
+        decimal Quantity,
+        decimal PriceAtPurchase,
+        DateTime PurchaseDate
+    );
+}
+
